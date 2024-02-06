f1c30a556bd608c5ebdd71cfea332aa0c32b7fdb
LSAfrika <lstudiosafrika@gmail.com>
2024-02-06 14:02:54 +0300

    finished team stats mobile ui

--- src/app/routes/teamstats/teamstats.component.ts
+++ src/app/routes/teamstats/teamstats.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { NavComponent } from '../../components/nav/nav.component';
 import { Router } from '@angular/router';
 import { NavigationService } from '../../services/navigation.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-teamstats',
   standalone: true,
-  imports: [NavComponent],
+  imports: [CommonModule],
   templateUrl: './teamstats.component.html',
   styleUrl: './teamstats.component.scss'
 })
@@ -15,9 +16,16 @@ export class TeamstatsComponent {
   private router=inject(Router)
   private navi=inject(NavigationService)
 
+  seasons=['23/24','22/23']
+active=4
   constructor(){
     this.navi.currentroute=this.router.url
 
+  }
+
+  activetab(select:number){
+
+    this.active=select
   }
 
 }
